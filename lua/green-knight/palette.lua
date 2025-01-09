ab15934f4eb113e824cc2ea3658fd7eea1e9c486
Iggy Kuperson <i.kupchinenko@gmail.com>
2025-01-09 23:19:29 +0300

    less shit

--- lua/green-knight/palette.lua
+++ lua/green-knight/palette.lua
@@ -1,9 +1,12 @@
 local palette = {
 	green = "#0D1E15", -- background
+	green_dark = "#091910", -- background
 	green_light = "#1D2C24", -- ui
-	green_pale = "#566A5B", -- string
+	green_light2 = "#2F453A", -- ui
+	green_pale = "#788E7E", -- string
+	green_vibrant = "#24A560", -- comment
 	orange = "#9D713E", -- functions
-	blue = "#647682", -- functions, but could be other shade of green or orange or red
+	blue = "#647682", -- parameters, but could be other shade of green or orange or red
 	white = "#CEC4B8", -- text, dune.
 	violet = "#968A98", -- constant, numbers
 	yellow = "#C1AF89", -- types, or maybe just use another shade of green
