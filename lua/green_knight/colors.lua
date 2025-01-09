8927af72250f1b011ad1b77a4f23dfd7878c6095
Iggy Kuperson <i.kupchinenko@gmail.com>
2025-01-09 21:01:15 +0300

    first phase of green

---
+++ lua/green_knight/colors.lua
@@ -0,0 +1,10 @@
+local palette = {
+	green = "#0D1E15", -- background
+	greenLight = "#2D4032", -- ui
+	greenPale = "#566A5B", -- string
+	orange = "#9D713E", -- functions
+	blue = "#647682", -- functions, but could be other shade of green or orange or red
+	white = "#CEC4B8", -- text, dune.
+	violet = "#968A98", -- constant, numbers
+	yellow = "#C1AF89", -- types
+}
