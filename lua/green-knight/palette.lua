a3a80cd6764a92f467075d2597a6af8134d394f2
Iggy Kuperson <i.kupchinenko@gmail.com>
2025-01-10 13:09:38 +0300

    subtle

--- lua/green-knight/palette.lua
+++ lua/green-knight/palette.lua
@@ -1,19 +1,35 @@
 local palette = {
-	green = "#0D1F1C", -- background
-	green_dark = "#0A1614", -- dark accents
-	green_light = "#132C28", -- ui, light accents
-	green_light2 = "#1F3D38", -- ui
+	green = "#142321", -- background
+	green_dark = "#111817", -- dark accents
+	green_light = "#172523", -- ui, light accents
+	green_light2 = "#203330", -- ui
 	-- green_pale = "#788E7E", -- string
-	green_pale = "#549673", -- string
+	green_pale = "#8D9A7E", -- string
 	green_vibrant = "#1CB362", -- comment
 	orange = "#DF9F51", -- functions
-	blue = "#9ABED6", -- parameters, but could be other shade of green or orange or red
+	blue = "#7FA0B6", -- parameters, but could be other shade of green or orange or red
 	white = "#CEC4B8", -- text, dune.
 	violet = "#968A98", -- constant, numbers
 	yellow = "#C1AF89", -- types, or maybe just use another shade of green
 	gray = "#C1AF89", -- types, or maybe just use another shade of green
-	red = "#9B5050", -- errors
+	red = "#B97871", -- errors
 	none = "NONE", -- remove
+
+	-- green = "#0D1F1C", -- background
+	-- green_dark = "#0A1614", -- dark accents
+	-- green_light = "#132C28", -- ui, light accents
+	-- green_light2 = "#1F3D38", -- ui
+	-- -- green_pale = "#788E7E", -- string
+	-- green_pale = "#549673", -- string
+	-- green_vibrant = "#1CB362", -- comment
+	-- orange = "#DF9F51", -- functions
+	-- blue = "#9ABED6", -- parameters, but could be other shade of green or orange or red
+	-- white = "#CEC4B8", -- text, dune.
+	-- violet = "#968A98", -- constant, numbers
+	-- yellow = "#C1AF89", -- types, or maybe just use another shade of green
+	-- gray = "#C1AF89", -- types, or maybe just use another shade of green
+	-- red = "#9B5050", -- errors
+	-- none = "NONE", -- remove
 }
 
 return palette
